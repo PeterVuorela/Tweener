8ceba45c04b8aa0249829a7030b37e13d6d107e5
Peter Vuorela <vuorelapeter@gmail.com>
2015-06-22 15:55:41 +0300

    Added Example File

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Tweener
 C# Tweener For Unity Using Robert Penner's Easing Equations
+
+## Code Example
+See the Example File in **Scripts/** Folder.
---
+++ Scripts/Example.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/** 
+ * Example MonoBehaviour. Put it to a GameObject and give it some coordinates with startTweenTo();
+ */
+public class Example : MonoBehaviour
+{
+	private Tweener tweener = new Tweener();
+
+	public void startTweenTo( Vector3 to )
+	{
+		if(tweener!=null)
+		{
+			// Sets The Position From -> To
+			tweener.easeFromTo( transform.localPosition, to, 1f, Easing.BackEaseInOut, tweenComplete);
+		}
+	}
+
+	private void tweenComplete()
+	{
+		Debug.Log( "Tween Completed" );
+	}
+
+	void Update ()
+	{
+		if( tweener!=null && tweener.animating )
+		{
+			// Updates the Tweener
+			tweener.update();
+
+			// Set the Position
+			transform.localPosition = tweener.progression;
+
+			Debug.Log( "Progression Percentage: "+tweener.progressPct );
+			Debug.Log( "Progression Position: "+tweener.progression );
+		}
+		else
+		{
+			// Tween not init or its completed
+		}
+	}
+}
